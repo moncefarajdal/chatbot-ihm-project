b3e7f2c2d046c221baabd15c1fb2188f77c5b26e
start.vaadin.com <no-reply@vaadin.com>
2022-03-02 16:43:08 +0000

    Generated project

---
+++ src/main/java/com/chatbot/application/Application.java
@@ -0,0 +1,28 @@
+package com.chatbot.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "bankvirtualassistant")
+@PWA(name = "Bank Virtual Assistant", shortName = "Bank Virtual Assistant", offlineResources = {"images/logo.png"})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
